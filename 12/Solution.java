6cd32b6e8b88ed2dbfa53150c14f3e6f1918292a
tomasnyberg <tomas.nyberg7335@gmail.com>
2021-12-13 00:10:42 +0100

    finished day 12, edited template slightly

--- 12/Solution.java
+++ 12/Solution.java
@@ -3,44 +3,69 @@ import java.io.FileReader;
 import java.util.*;
 
 public class Solution {
+    public static int count;
+    public static long count1;
+    public static Map<String, Set<String>> edges = edgeMap();
     public static void main(String[] args){
         System.out.println(problemOne());
         System.out.println(problemTwo());
     }
     
     public static int problemOne(){
-        HashMap<String, Set<String>> edges = edgeMap();
-        traverse(edges, "", "start", 0, new HashSet<>());
-        return 0;
+        Map<String, Set<String>> edges = edgeMap();
+        traverse(edges, "", "start", new HashSet<>());
+        return count;
     }
 
-    public static void traverse(HashMap<String, Set<String>> edges, String path, String curr, int smallVisited, Set<String> visited){
+    public static void traverse(Map<String, Set<String>> edges, String path, String curr, Set<String> visited){
         if(curr.equals("end")){
-            System.out.println("Found path: " + path + ", end");
+            count++;
             return;
         }
-        visited.add(curr);
         path += ", " + curr;
-        // TODO: check for small caves, and also make it possible to visit nodes twice so long as they have a new unvisited node
-
+        boolean small = curr.charAt(0) >= 97 && curr.charAt(0) <= 122;
+        if(small){
+            visited.add(curr);
+        }
         for(String s: edges.get(curr)){
-            //might need to create new set??
-            Set<String> sset = new HashSet<>(edges.get(s));
             if(!visited.contains(s)){
-                // System.out.println("current path: " + path);
-                traverse(edges, path, s, smallVisited, visited);
+                traverse(edges, path, s, visited);
             }
         }
         visited.remove(curr);
     }
-
-    public static int problemTwo(){
-        return 0;
+    public static void traverseTwo(String a, Set<String> seen, boolean can_twice){
+        if(a.equals("end")){
+            count1++;
+            return;
+        }
+        for(String b: edges.get(a)){
+            if(b.toLowerCase().equals(b)){
+                if(!seen.contains(b)){
+                    Set<String> newSet = new HashSet<>(seen);
+                    newSet.add(b);
+                    traverseTwo(b, newSet, can_twice);
+                } else if (can_twice && !(b.equals("start") || b.equals("end"))) {
+                    Set<String> newSet = new HashSet<>(seen);
+                    newSet.add(b);
+                    traverseTwo(b, seen, false);
+                }
+            } else {
+                traverseTwo(b, seen, can_twice);
+            }
+        }
     }
 
-    public static HashMap<String, Set<String>> edgeMap(){
-        ArrayList<String> list = parseInputToArray();
-        HashMap<String, Set<String>> edges = new HashMap<>();
+    public static long problemTwo(){
+        HashSet<String> set = new HashSet<>();
+        set.add("start");
+        traverseTwo("start", set, true);
+        return count1;
+    }
+
+    public static Map<String, Set<String>> edgeMap(){
+        List<String> list = parseInputToArray();
+        Map<String, Set<String>> edges = new HashMap<>();
         for(String s: list){
             String a = s.split("-")[0];
             String b = s.split("-")[1];
@@ -53,17 +78,17 @@ public class Solution {
         }
         //check that the edge map is correct
         // for(var entry: edges.entrySet()){
-        //     System.out.print("From: " + entry.getKey() + " ");
-        //     System.out.print("To: " + entry.getValue());
-        //     System.out.println();
+            // System.out.print("From: " + entry.getKey() + " ");
+            // System.out.print("To: " + entry.getValue());
+            // System.out.println();
         // }
         return edges;
     }
 
-    public static ArrayList<String> parseInputToArray(){
+    public static List<String> parseInputToArray(){
         try {
-            BufferedReader reader = new BufferedReader(new FileReader("input1.txt"));
-            ArrayList<String> list = new ArrayList<>();
+            BufferedReader reader = new BufferedReader(new FileReader("input.txt"));
+            List<String> list = new ArrayList<>();
             String line = reader.readLine();
             while(line != null){
                 list.add(line);
