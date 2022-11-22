e5b8e0300a5d86ff5c83ee0e5ffb4d73a2c16d5f
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-11-22 16:32:07 +0100

    solve day 2 in cpp

--- 2/2.cpp
+++ 2/2.cpp
@@ -17,6 +17,23 @@ int part_one(vector<pair<string, int>> &commands) {
     return x*y;
 }
 
+int part_two(vector<pair<string, int>> &commands) {
+    int x = 0;
+    int y = 0;
+    int aim = 0;
+    for (int i = 0; i < commands.size()-1; i++) {
+        if(commands[i].first[0] == 'f'){
+            x += commands[i].second;
+            y += aim*commands[i].second;
+        } else if (commands[i].first[0] == 'd'){
+            aim += commands[i].second;
+        } else if (commands[i].first[0] == 'u'){
+            aim -= commands[i].second;
+        }
+    }
+    return x*y;
+}
+
 int main() {
     vector<pair<string, int>> commands; 
     while (cin) {
@@ -24,8 +41,8 @@ int main() {
         int n;
         cin >> s;
         cin >> n;
-        // add s and n as a pair to commands
         commands.push_back(make_pair(s, n));
     }
     cout << "Part one answer: " << part_one(commands) << endl;
+    cout << "Part two answer: " << part_two(commands) << endl;
 }
