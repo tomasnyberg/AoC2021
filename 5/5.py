d01065ee0646207563dcd938f301d4295bdfabf0
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-11-27 18:56:34 +0100

    solve day 5 part 2 in python

--- 5/5.py
+++ 5/5.py
@@ -11,17 +11,25 @@ def check():
                 result += 1
     return result
 
-
 for line in lines:
     a, b = line.split(" -> ")
     x1, y1, = map(int,a.split(","))
     x2, y2, = map(int,b.split(","))
     if x1 == x2 or y1 == y2:
-        print(x1,x2, y1,y2)
         for i in range(min(x1, x2), max(x1, x2)+1):
             for j in range(min(y1, y2), max(y1, y2)+1):
                 matrix[j][i] += 1
-
+    else:
+        coords = [[x1, y1], [x2,y2]]
+        coords.sort()
+        if coords[0][1] <= coords[1][1]:
+            while coords[0][0] <= coords[1][0]:
+                matrix[coords[0][1]][coords[0][0]] += 1
+                coords[0][0] += 1
+                coords[0][1] += 1
+        else:
+            while coords[0][0] <= coords[1][0]:
+                matrix[coords[0][1]][coords[0][0]] += 1
+                coords[0][0] += 1
+                coords[0][1] -= 1
 print(check())
-
-
