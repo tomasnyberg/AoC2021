e51fb7e3e02d2925087f87934283c30e7eed6d5b
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-11-27 19:02:47 +0100

    slight cleanup of 5 in python

--- 5/5.py
+++ 5/5.py
@@ -1,9 +1,8 @@
 import sys
 lines = list(map(str.strip, sys.stdin.readlines()))
 
-matrix = [[0 for _ in range(2000)] for _ in range(2000)]
 
-def check():
+def check(matrix):
     result = 0
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
@@ -11,25 +10,31 @@ def check():
                 result += 1
     return result
 
+def solve(coords, horizontal):
+    matrix = [[0 for _ in range(1002)] for _ in range(1002)]
+    for x1, y1, x2, y2 in coords:
+        if x1 == x2 or y1 == y2:
+            for i in range(min(x1, x2), max(x1, x2)+1):
+                for j in range(min(y1, y2), max(y1, y2)+1):
+                    matrix[j][i] += 1
+        elif horizontal:
+            coords = [[x1, y1], [x2,y2]]
+            coords.sort()
+            inc = 1 if coords[0][1] <= coords[1][1] else -1
+            while coords[0][0] <= coords[1][0]:
+                matrix[coords[0][1]][coords[0][0]] += 1
+                coords[0][0] += 1
+                coords[0][1] += inc
+    return check(matrix)
+
+coords = []
+
 for line in lines:
     a, b = line.split(" -> ")
     x1, y1, = map(int,a.split(","))
     x2, y2, = map(int,b.split(","))
-    if x1 == x2 or y1 == y2:
-        for i in range(min(x1, x2), max(x1, x2)+1):
-            for j in range(min(y1, y2), max(y1, y2)+1):
-                matrix[j][i] += 1
-    else:
-        coords = [[x1, y1], [x2,y2]]
-        coords.sort()
-        if coords[0][1] <= coords[1][1]:
-            while coords[0][0] <= coords[1][0]:
-                matrix[coords[0][1]][coords[0][0]] += 1
-                coords[0][0] += 1
-                coords[0][1] += 1
-        else:
-            while coords[0][0] <= coords[1][0]:
-                matrix[coords[0][1]][coords[0][0]] += 1
-                coords[0][0] += 1
-                coords[0][1] -= 1
-print(check())
+    coords.append([x1,y1,x2,y2])
+
+print("Part one answer: ", solve(coords, False))
+print("Part two answer: ", solve(coords, True))
+
